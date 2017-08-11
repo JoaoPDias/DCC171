2c7f199b5893bc403f102a57644b741ec1a00d7b
João <jpdias1997@hotmail.com>
2017-08-10 21:57:25 -0300

    Atualizaçao

--- src/br/ufjf/dcc171/Aula02.java
+++ src/br/ufjf/dcc171/Aula02.java
@@ -5,12 +5,18 @@
  */
 package br.ufjf.dcc171;
 
+import javax.swing.JFrame;
+
 public class Aula02 {
 
     public static void main(String[] args) {
         Janela janela = new Janela();
-        janela.setVisible(true);
 
+        janela.setSize(320, 200);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLocationRelativeTo(null);
+
+        janela.setVisible(true);
     }
 
 }
